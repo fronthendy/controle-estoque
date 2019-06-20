300ae7d8d5c40bf59ea08cb60494e117b41fb367
fronthendy <hendy.almeida@hotmail.com>
2019-06-20 19:22:48 -0300

    correção do bug - altera imagem do produto

--- alterar-produto.php
+++ alterar-produto.php
@@ -27,41 +27,51 @@ if (isset($_GET['pos'])) {
 <main class="container">
     <h1>Alterar produto</h1>
 
-    <form action="index.php" method="POST" autocomplete="off">
-        <div class="form-group">
-            <label for="nome">Nome</label>
-            <input type="text" name="nome" class="form-control" value="<?php echo $produto['nome'] ?>">
+    <form action="index.php" method="POST" autocomplete="off" enctype="multipart/form-data">
+        <div class="row">
+        <div class="col-md-6">
+            <div class="form-group">
+                <label for="nome">Nome</label>
+                <input type="text" name="nome" class="form-control" value="<?php echo $produto['nome'] ?>">
+            </div>
+            <div class="form-group">
+                <label for="preco">Preço</label>
+                <input type="number" name="preco" class="form-control" value="<?php echo $produto['preco'] ?>">
+            </div>
+            <div class="form-group">
+                <label for="qtd">Quantidade em estoque</label>
+                <input type="number" name="estoque" class="form-control" value="<?php echo ($produto['estoque']) ?>">
+            </div>
+            <div class="form-group">
+                <label for="qtd-minima">Quantidade minima</label>
+                <input type="number" name="min" class="form-control" value="<?php echo ($produto['min']) ?>">
+            </div>
+            <div class="form-group">
+                <label for="status">Ativo </label>
+                <input type="checkbox" name="status" value="1" <?php echo (isset($produto['status']) ? 'checked' : ''); ?>>
+            </div>
+
+            <div class="form-group">
+                <label for="foto">Nova foto do produto</label>
+                <input type="file" name="foto" id="foto" class="form-control">
+            </div>
+
+            <input type="hidden" name="pos" value="<?= $pos ?>">
         </div>
-        <div class="form-group">
-            <label for="preco">Preço</label>
-            <input type="number" name="preco" class="form-control" value="<?php echo $produto['preco'] ?>">
-        </div>
-        <div class="form-group">
-            <label for="qtd">Quantidade em estoque</label>
-            <input type="number" name="estoque" class="form-control" value="<?php echo ($produto['estoque']) ?>">
-        </div>
-        <div class="form-group">
-            <label for="qtd-minima">Quantidade minima</label>
-            <input type="number" name="min" class="form-control" value="<?php echo ($produto['min']) ?>">
-        </div>
-        <div class="form-group">
-            <label for="status">Status</label>
-            <input type="checkbox" name="status" value="1" <?php echo (isset($produto['status']) ? 'checked' : ''); ?>>
-        </div>
-        <?php
-        var_dump($produto);
-        if (isset($produto['imagem'])) {
-            echo "<img src='" . $produto['imagem'] . "'>";
-        }
-        ?>
-        <div class="form-group">
-            <label for="foto">Foto do produto</label>
-            <input type="file" name="foto" id="foto" class="form-control">
-        </div>
+        <div class="col-md-3">
+
+            <?php
+
+            if (isset($produto['imagem'])) {
+                echo "<label>Foto atual do produto</label>";
+                echo "<img src='" . $produto['imagem'] . "' class='img-responsive'>";
+            }
+            ?>
 
 
-        <input type="hidden" name="pos" value="<?= $pos ?>">
+        </div>
 
+        <div class="col-xs-12">
         <div class="form-group">
             <button type="submit" class="btn btn-primary" name="alterar-produto">Salvar</button>
         </div>
@@ -72,6 +82,8 @@ if (isset($_GET['pos'])) {
             echo "</div>";
         }
         ?>
+        </div>
+        </div>
     </form>
 </main>
 
